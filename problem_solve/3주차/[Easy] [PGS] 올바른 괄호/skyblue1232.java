70ca75b5714bf68f62b98195fdd66462becc903f
고민균 <skybluekmg@gmail.com>
2026-07-23 14:20:53 +0900

    올바른 괄호

--- problem_solve/3주차/[Easy] [PGS] 올바른 괄호/skyblue1232.java
+++ problem_solve/3주차/[Easy] [PGS] 올바른 괄호/skyblue1232.java
@@ -1,10 +1,39 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution("()()"));    // true
+        System.out.println(sol.solution("(())()"));  // true
+        System.out.println(sol.solution(")()("));    // false
+        System.out.println(sol.solution("(()("));    // false
+    }
+}
+
+class Solution {
+
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i=0; i<s.length(); i++) {
+        	char c = s.charAt(i);
+        	
+        	if(c=='(') {
+        		stack.push(c);
+        	} else {
+        		
+        		if(stack.isEmpty()) {
+        			return false;
+        		}
+        		
+        		stack.pop();
+        	}
+        }
+
+        return stack.isEmpty();
     }
 }
