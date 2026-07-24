166681e7b75cadbd5a06d7e9859d53756d2305e0
mojunsul <sdg2435@naver.com>
2026-07-24 13:35:38 +0900

    도둑질 최적화

--- problem_solve/3주차/[Hard] [PGS] 도둑질/mojunsul.java
+++ problem_solve/3주차/[Hard] [PGS] 도둑질/mojunsul.java
@@ -1,34 +1,20 @@
 class Solution {
-	
-	static int[] dp1;
-	static int[] dp2;
-	static int N;
-	static int[] money;
-	
-    public int solution(int[] input) {
-    	money = input;
-        N = money.length;
-        dp1 = new int[N]; // [idx까지 확인했을 때][0: 안털림, 1: 털림][시작점 털림 유무]
-        dp2 = new int[N];
-        
-        //시작 안턺
-        int sum = 0;
-        for(int i=1; i<N; i++) {
-        	dp1[i] = Math.max(dp1[i-1], dp2[i-1]);
-        	dp2[i] = dp1[i-1] + money[i];
+		
+    public int solution(int[] money) {
+    	int N = money.length;
+        int[][] dp = new int[2][N];
+
+        dp[0][0] = 0;
+        dp[0][1] = money[1];
+        dp[1][0] = money[0];
+        dp[1][1] = money[0];
+
+        for(int idx=2; idx<N; idx++) {
+        	for(int isFirstRobbed=0; isFirstRobbed<2; isFirstRobbed++) {
+        		dp[isFirstRobbed][idx] = Math.max(dp[isFirstRobbed][idx-2] + money[idx], dp[isFirstRobbed][idx-1]);
+        	}
         }
-        int ans = Math.max(dp1[N-1], dp2[N-1]);
         
-        //시작 턺
-        dp1 = new int[N];
-        dp2 = new int[N];
-        dp2[0] = money[0];
-        sum = 0;
-        for(int i=1; i<N-1; i++) {
-        	dp1[i] = Math.max(dp1[i-1], dp2[i-1]);
-        	dp2[i] = dp1[i-1] + money[i];
-        }
-        ans = Math.max(ans, Math.max(dp1[N-2], dp2[N-2]));
-        return ans;
+        return Math.max(dp[0][N-1], dp[1][N-1]);
     }
 }
